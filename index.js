983acb82331a30a44f11b475b94431d1e3e3ea00
GummiCode <gummicodeblog@gmail.com>
2020-05-26 23:42:29 +0100

    Added JS to insert an HTML paragraph into index.html, which states how many people are currently in space. Adjusted the numSpaceFrens variable to have a value between 0 and 5.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 console.log('JS is connected.');
 
 //numSpaceFrens will be replaced with an array containing the names of people in space.
-const numSpaceFrens = Math.floor(Math.random()*5)+1;
+const numSpaceFrens = Math.floor(Math.random()*6);
 const frenSpacingDegrees = 360/numSpaceFrens;
 
 const addFren = (axisNumber) => {
@@ -10,10 +10,17 @@ const addFren = (axisNumber) => {
       <img id="spacefren" src="assets/images/SpaceFren.png">
     </div>`;
 };
-
 //the variable numSpaceFrens here will be replaced with the length of the array of people in space.
 for (i = 0; i < numSpaceFrens; i++) {
   addFren();
+};
+
+if (numSpaceFrens === 1) {
+  document.getElementById(`info-relay`).innerHTML = `<p>There is currently ${numSpaceFrens} person in space!</p>` 
+} else if (numSpaceFrens === 0) {
+  document.getElementById(`info-relay`).innerHTML = `<p>There are currently no people in space.</p>`
+} else {
+  document.getElementById(`info-relay`).innerHTML = `<p>There are currently ${numSpaceFrens} people in space!</p>`
 };
 
 console.log(numSpaceFrens);
