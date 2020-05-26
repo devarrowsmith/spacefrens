94d99c57f18b4ada93dab4e8679df82058a5548f
GummiCode <gummicodeblog@gmail.com>
2020-05-26 23:24:28 +0100

    Inserted the index.js script into the index.html script. Scripted JS to assign a random number of astronauts to show (1 to 5) then insert that number of copies of the SpaceFren png image into the HTML, so that the webpage shows that number of stronauts. it works.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log(screen.width);
+console.log('JS is connected.');
+
+//numSpaceFrens will be replaced with an array containing the names of people in space.
+const numSpaceFrens = Math.floor(Math.random()*5)+1;
+const frenSpacingDegrees = 360/numSpaceFrens;
+
+const addFren = () => {
+  document.getElementById(`spacefrens`).innerHTML += `<img id="spacefren" src="assets/images/SpaceFren.png">`;
+};
+
+//the variable numSpaceFrens here will be replaced with the length of the array of people in space.
+for (i = 0; i < numSpaceFrens; i++) {
+  addFren();
+};
+
+console.log(numSpaceFrens);
+console.log(frenSpacingDegrees);
