f0a3f283a2efe96d1534079c4697b9fcee63ec99
GummiCode <gummicodeblog@gmail.com>
2020-05-29 20:06:08 +0100

    Due to security issue when deployed (API used HTTP rather than HTTPS), reformatted the directory for static serving using Express & depolyed on Heroku. The app will now be served from an HTTP server, negating the cross-origin request security issue.

--- index.js
+++ index.js
@@ -1,58 +1,7 @@
-console.log("JS is connected.");
-const sheet = document.styleSheets[0];
-console.log(sheet);
-let angleAdjust = 0;
+const app = require('./src/app');
 
-window
-  .fetch(`http://api.open-notify.org/astros.json`)
-  .then((res) => res.json())
-  .then((data) => {
-    console.log(data);
-    const numSpaceFrens = data.number;
-    const people = data.people;
-    const frenSpacingDegrees = 360 / numSpaceFrens;
-    const addFren = (axisNumber) => {
-      document.getElementById(
-        `spacefrens`
-      ).innerHTML += `<div class= "space_fren_axis" id="axis${axisNumber}">
-          <img id="spacefren" src="assets/images/SpaceFren.png">
-        </div>`;
-      angleAdjust += frenSpacingDegrees;
-      document.getElementById(
-        `axis${axisNumber}`
-      ).style.transform = `rotate(${angleAdjust}deg)`;
-      document.getElementById(
-        `axis${axisNumber}`
-      ).style.webkitAnimation = `spin${axisNumber} 5s linear infinite`;
-      sheet.insertRule(
-        `@-webkit-keyframes spin${axisNumber} { 0% { -webkit-transform: rotate(${
-          0 + angleAdjust
-        }deg);} 100% { -webkit-transform:rotate(${360 + angleAdjust}deg);} }`,
-        sheet.cssRules.length
-      );
-    };
-    for (i = 0; i < numSpaceFrens; i++) {
-      addFren(i + 1);
-    }
+const APP_PORT = process.env.PORT || 8000;
 
-    people.forEach(
-      (person) =>
-        (document.getElementById(
-          `infobox`
-        ).innerHTML += `<p>${person.name}:<br>${person.craft}</p>`)
-    );
-
-    if (numSpaceFrens === 1) {
-      document.getElementById(
-        `info-relay`
-      ).innerHTML = `<p>There is currently ${numSpaceFrens} person in space!</p>`;
-    } else if (numSpaceFrens === 0) {
-      document.getElementById(
-        `info-relay`
-      ).innerHTML = `<p>There are currently no people in space.</p>`;
-    } else {
-      document.getElementById(
-        `info-relay`
-      ).innerHTML = `<p>There are currently ${numSpaceFrens} people in space!</p>`;
-    }
-  });
+app.listen(APP_PORT, () => {
+  console.log(`Now serving your Express app at http://localhost:${APP_PORT}`); // eslint-disable-line
+});
